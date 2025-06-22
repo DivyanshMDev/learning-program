9bfee75aef340ae55c8e6a042ab9d68452b04f43
DivyanshMDev <22052813@kiit.ac.in>
2025-06-22 18:40:30 +0530

    Add Singleton Pattern example and solution/project files

---
+++ WEEK-1/Design Patterns and Principles/SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Get the singleton instance and use it
+        Logger logger1 = Logger.Instance;
+        Logger logger2 = Logger.Instance;
+
+        logger1.Log("Hello, Singleton!");
+
+        
+        if (logger1 == logger2)
+        {
+            Console.WriteLine("Both loggers are the same instance!");
+        }
+        else
+        {
+            Console.WriteLine("Loggers are different instances!");
+        }
+    }
+}
