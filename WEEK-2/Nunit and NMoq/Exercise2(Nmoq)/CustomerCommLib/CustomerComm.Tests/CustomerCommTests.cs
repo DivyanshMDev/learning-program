cb82909a9ec130c9bca33b88119b1a5facbc33cc
DivyanshMDev <22052813@kiit.ac.in>
2025-06-29 13:30:51 +0530

    Add Week 2 Exercise 2: NMoq mocking framework implementation

---
+++ WEEK-2/Nunit and NMoq/Exercise2(Nmoq)/CustomerCommLib/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture] ]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerCommLib.CustomerComm _customerComm;
+
+        [OneTimeSetUp] 
+        public void OneTimeSetUp()
+        {
+            
+            _mockMailSender = new Mock<IMailSender>();
+
+          
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+        }
+
+        [TestCase] 
+        public void SendMailToCustomer_ShouldReturnTrue_WhenMailSenderReturnsTrue()
+        {
+          
+            _mockMailSender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                          .Returns(true);
+
+         
+            bool result = _customerComm.SendMailToCustomer();
+
+           
+            Assert.That(result, Is.True); 
+
+           
+            _mockMailSender.Verify(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()),
+                                  Times.Once);
+        }
+    }
+}
---
+++ WEEK-2/Nunit and NMoq/Exercise2(Nmoq)/CustomerCommLib/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private IMailSender _mailSender;
+
+        // Constructor Injection - we inject the dependency here
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            // Actual logic goes here
+            // Define message and mail address
+            bool result = _mailSender.SendMail("cust123@abc.com", "Some Message");
+
+            return result;
+        }
+    }
+}
